6a650b25e074ea6c3dc033eb39b521c9a1348e50
Nathan Lee <nlee806@gmail.com>
2018-05-12 22:11:56 -0700

    Added files.

---
+++ cookie-clicker-lab4-7/cookie-clicker-lab4-7/cookie-clicker-lab4-7incorrect/src/main/java/edu/csula/web/AdminEventsServlet.java
@@ -0,0 +1,70 @@
+package edu.csula.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.csula.storage.servlet.EventsDAOImpl;
+import edu.csula.storage.EventsDAO;
+import edu.csula.models.Event;
+
+@WebServlet("/admin/events")
+public class AdminEventsServlet extends HttpServlet {
+	@Override
+	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> data = dao.getAll();
+    	request.setAttribute("data", data);
+		request.getRequestDispatcher(${"admin-events.jsp"})
+			.forward(request, response);
+	} 
+	//	response.setContentType("text/html");
+	//	PrintWriter out = response.getWriter();
+		// TODO: render the events page HTML
+	//	ArrayList<Entry> entries = (ArrayList<Entry>) getServletContext().getAttribute("events");
+//		html +="<form>";
+	//	EventsDAO dao = new EventsDAOImpl(getServletContext());
+	//	Collection<Event> events = dao.getAll();
+	//	System.out.println(events);
+	//	out.println("<h1>events servlet</h1>");
+//		html +=”<tr>”
+//		for(Entry entry: entries){
+//			html += entry.getAll();
+//		}
+//		html += "<form>";
+//		html +=”</table>”
+//		html +=”<form>”
+//		html +=”<h2>Add comment</h2>”
+//		html +=”<input name = ‘name’>”
+//		html +=”<button>Submit<button>”
+//		html +=”</td><td><a href=’/entries/edit?id=” +entry.getAll = ”>”
+	//}
+//public void init(){
+//entries.add(new Entry(entries.size(), ))
+//}
+	@Override
+	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO: handle upsert transaction
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> entries = dao.getAll();
+	//	ArrayList<GuestbookEntry> entries = (ArrayList<GuestBookEntry>) getServletContext().getAttribute(“guestbook-entries”);
+	//parsing
+		//String username = request.getParameter(“username”);
+//String username = request.getParameter("event-1");
+		//String comment = request.getParameter(“comment”);
+//String comment = request.getParameter("generator-1");
+//		Event entry = new Event()entries.size(), username), comment);
+	dao.add(entries);
+	response.sendRedirect("/events");
+//		entries.add(entry);
+//		getServletContext().setAttribute(“”,entries);
+//		response.sendRedirect(“/guestbook”);
+//		doGet(request, response);
+	}
+}
---
+++ cookie-clicker-lab4-7/cookie-clicker-lab4-7/src/main/java/edu/csula/storage/schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE users (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE generators(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    rate INTEGER, 
+    base_cost INTEGER,
+    unlock_at INTEGER,
+    created_by INTEGER,
+    FOREIGN KEY (created_by) REFERENCES users(id)
+);
+
+CREATE TABLE events(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    trigger_at INTEGER,
+    created_by INTEGER,
+    FOREIGN KEY (created_by) REFERENCES users(id)
+);
+
+CREATE TABLE quantities(
+	generator_id INTEGER NOT NULL,
+    token VARCHAR(255),
+    quantity INTEGER,
+    CONSTRAINT random UNIQUE (generator_id, token)
+);
+
+INSERT INTO users VALUES (1, 'admin', 'cs3220password');
+INSERT INTO users VALUES (2, 'me', 'notapassword');
+
+INSERT INTO generators VALUES (1, 'Grandma', 'Grandma likes to make cookies', 5, 10, 10, 1);
+INSERT INTO generators VALUES (2, 'Factory', 'Factory to produce cookies', 10, 50, 50, 1);
+INSERT INTO generators VALUES (3, 'Mine', 'Mining cookies', 20, 200, 200, 2);
+
+INSERT INTO events VALUES (1, 'Grandma shows up', 'You always know grandma likes to make cookies', 10, 1);
+INSERT INTO events VALUES (2, 'You can construct factory now!','Factory to produce cookies', 50, 1);
+INSERT INTO events VALUES (3, 'We''ve found cookies in deep mountain ... in the mine?','Mining cookies', 200, 2);
+INSERT INTO events VALUES (4, 'sample event', 'This is a sample event. Please delete me', 99999, 2);
+
+INSERT INTO quantities VALUES (1, 'c7a69d44e0b9b415b2d9956cb26b944a', 2);
+INSERT INTO quantities VALUES (2, 'c7a69d44e0b9b415b2d9956cb26b944a', 1);
+INSERT INTO quantities VALUES (1, '80516ce4663c3bd0c8385309a2fe226e', 20);
+INSERT INTO quantities VALUES (2, '80516ce4663c3bd0c8385309a2fe226e', 30);
